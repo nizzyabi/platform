a94d967431db099d337ab2f31beab2fa5a865d7f
NizarAbiZaher <Nizabizaher@gmail.com>
2023-12-29 07:43:38 -0500

    crazy change

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -26,100 +26,43 @@ export default function LandingPage() {
 
 
   return (
-    <div>
-    
-      {/* Intro Bar */}
-        <div className='flex flex-col md:flex-row justify-center md:justify-between items-start mx-8 md:mx-[120px] mt-[10px] bg-transparent'>
-        {/* Intro Text */}
-        <div className='text-center mt-8 md:mt-0 md:pr-10 w-full md:max-w-xl lg:max-w-2xl md:self-center'>
-            
-            <h1 data-aos="fade-right" className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold px-2'>learn to <span className='text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-600'>code</span> & have <span className='text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-teal-500'>fun</span> doing it.</h1>
-          
-            {/* Start Button */}
-            <Link href='/courses'>
-                <Button className='font-extrabold rounded hover:bg-purple-500 hover:scale-105 transition-transform text-xl bg-purple-500 text-white mt-[20px]' data-aos="fade-right">
-                Let's Start <LocalFireDepartmentIcon className='bg-transparent ml-2'/>
-                </Button>
-            </Link>
-        </div>
+    <div className='mt-12'>
 
-        {/* Video */}
-        <div className='w-full md:w-auto flex mr-12 justify-center items-center mt-10'>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/KngzC93n19Q?si=RLTb9Crp21_htsEy" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className=' shadow-lg shadow-violet-500' data-aos="fade-left"></iframe>
-        </div>
-      
-        </div>
-          
-          {/* Icons 
+      {/* Landing */}
+      <div>
+        {/* Text + Button */}
         <div>
-          <Icons data-aos='fade-up'/>
-        </div>
-        */}
-        
-        {/* Arrow ref={ref7}*/}
-        <div className={`transition-all duration-100 flex items-center justify-center pt-[150px]`}>
-          <p data-aos="fade-right" className='text-6xl arrowDown'>⏬</p>
-        </div>
+          {/* Main text*/}
+          <div>
+            <h1>Become a Software Developer</h1>
+          </div>
+          {/* Sub text */}
+          <div>
+            <p>Learn to code and have fun doing it.</p>
+          </div>
 
-        {/* Questions & Answers */}
-        <div className='space-x-12 px-12 pt-[80px]'>
-          
-        {/* 1 */}
+          {/* Button */}
+          <div>
+            <Button>Let's Start</Button>
+          </div>
+        </div>
+        {/* Icon Display*/}
         <div>
-          <div className='flex items-center justify-center mt-[250px]'>
-            <h1 className='rounded px-2 py-1.5 outline-dashed outline-red-500 font-extrabold text-6xl text-red-500 text-center' data-aos='fade-right' >Why Am I Struggling 😣</h1>
-          </div>
-          <div className='flex items-center justify-center'>
-            <h1 className='mt-[100px] text-center text-3xl font-extrabold' data-aos='fade-left'>You only retain <span className='text-5xl text-purple-500'>5% - 10%</span> of what you read or watch, which is why you're struggling...</h1>
-          </div>
-          {/* Add Pyramid Here*/}
-          <div className='flex justify-center items-center' data-aos='fade-up'>
-            <Image alt='pyramid' src='/pyramid.svg' width={900} height={500} className='flex items-center justify-center pt-[100px]'/>
-          </div>
-          <div className='flex items-center justify-center pt-[100px]'>
-            <p data-aos='fade-up'className='text-6xl arrowDown'>⏬</p>
+          <div>
+            {/*cards*/}
+            <div className=''></div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
         </div>
+      </div>
 
-        {/* 2 */}
-        <div>
-          <div className='flex items-center justify-center mt-[250px]'>
-            <h1 data-aos='fade-right' className='rounded px-2 py-1.5 outline-dashed outline-blue-500 font-extrabold text-6xl text-blue-500 text-center'>Reality Check 😬</h1>
 
-            {/* Add Pictures Here*/}
+      {/* Description */}
 
-          </div>
-          <div className='flex items-center justify-center'>
-            <h1 data-aos='fade-left' className='mt-[100px] text-center text-3xl font-extrabold'>It is <span className='text-orange-500 text-5xl'>IMPOSSIBLE</span> to learn code by watching videos and assisted work...I tried </h1>
-          </div>
-          <div className='transition-all duration-100 flex items-center justify-center pt-[100px]'>
-            <p data-aos='fade-up' className='text-6xl arrowDown'>⏬</p>
-          </div>
-        </div>
-          
-        {/* 3 */}
-        <div>
-          <div className='transition-all duration-500 flex items-center justify-center mt-[250px]'>
-            <h1 data-aos='fade-right' className='rounded px-2 py-1.5 outline-dashed outline-purple-500 font-extrabold text-6xl text-purple-500 text-center'>The Answer 🧑‍💻</h1>
-          </div>
-          <div className='transition-all duration-500 flex items-center justify-center'>
-            <h1 data-aos='fade-left' className='mt-[100px] text-center text-3xl font-bold pt-6'>The only way to learn code successfully is through <span className='text-5xl text-pink-500'>Project Based Learning</span> ( PBL). You retain 75% - 80% of what you learning using PBL. </h1>
-          </div>
-          <div className='flex items-center justify-center pt-[30px] ml-[20px]'>
-            <Lottie animationData={arrow} className='w-[250px] h-[250px]' loop={false}/>
-         </div>
-        </div>
 
-        {/* Button */}
-        <div className='flex items-center justify-center'>
-          <Link href='/courses'>
-                <Button className='font-extrabold rounded hover:bg-purple-500 hover:scale-105 transition-transform text-xl bg-purple-500 text-white mt-[20px]'>
-                Let's Start <LocalFireDepartmentIcon className='bg-transparent ml-2'/>
-                </Button>
-            </Link>
-        </div>
-        
-        </div>
+       
         
         {/* Footer */}
         <Footer />
