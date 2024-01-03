e401d0902d75eb12a8145d98bdaa9d9fabfa53dd
NizarAbiZaher <Nizabizaher@gmail.com>
2024-01-03 09:34:44 -0500

    register hash

--- actions/register.ts
+++ actions/register.ts
@@ -3,7 +3,7 @@
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
 import bcrypt from "bcrypt";
-
+import { db } from "@/lib/db";
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     // Validate fields
     const validatedFields = RegisterSchema.safeParse(values);
@@ -12,6 +12,33 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if(!validatedFields.success) {
         return { error: "Invalid fields 😞"};
     }
+
+    // extract validated fields
+    const { email, password, name } = validatedFields.data;
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // confirm email is not taken
+    const exisitingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    })
+    // display text if email is taken
+    if (exisitingUser) {
+        return { error: "Email already taken 😞"}
+    }
+    // succes code
+    await db.user.create({
+        data: {
+            name,
+            email, 
+            password: hashedPassword,
+        },
+    })
+    //TODO: send verification email
+
     // If fields are valid
-    return { success: "Email sent 📫"}
+    return { success: "User created! 🎉"}
 };
