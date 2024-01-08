8b61e68f6e452a60e8cdf9a05603727f3d717fee
NizarAbiZaher <Nizabizaher@gmail.com>
2024-01-08 07:19:48 -0500

    verification form

--- components/auth/New-Verification-Form.tsx
+++ components/auth/New-Verification-Form.tsx
@@ -1,11 +1,26 @@
 "use client";
+
 import { ClipLoader } from "react-spinners";
 import { CardWrapper } from "@/components/auth/Card-Wrapper";
-
+import { useSearchParams } from "next/navigation"
+import { useCallback, useEffect } from "react";
 export const NewVerificationForm = () => {
+    // set search params & get token
+    const searchParams = useSearchParams();
+    const token = searchParams.get("token");
+
+    // onSubmit wihtin a useCallBack
+    const onSubmit = useCallback(() => {
+        console.log(token);
+    }, [token]);
+    // useEffect
+    useEffect(() => {
+        onSubmit();
+}, [onSubmit])
+
     return (
         <CardWrapper
-            headerTitle="Verify your email address"
+            headerTitle="verify your email 😏"
         >
             <div className="flex items-center w-full justify-center">
                 <ClipLoader color="white"/>
