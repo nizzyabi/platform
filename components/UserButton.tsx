95c64c29efdd358ed215c3a5f7b41cc5e1153dae
NizarAbiZaher <Nizabizaher@gmail.com>
2024-01-16 07:34:18 -0500

    User Button Session

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -1,22 +1,14 @@
 'use client'
 import { Button } from "@/components/ui/button"
-import {
-    Dialog,
-    DialogContent,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { RegisterForm } from "./auth/Register-Form"
-import { LoginForm } from "./auth/Login-Form"
 import { useState, useEffect } from "react"
 import { Avatar, CircularProgress } from "@mui/material"
 import Link from "next/link"
 import { useSession } from "next-auth/react"
 import { IoMdPerson } from "react-icons/io";
-import { ResetForm } from "./auth/Reset-Form"
 import { useRouter } from 'next/navigation';
+import { useCurrentUser } from "@/hooks/user-current-user"
 
 const UserButton = () => {
-    const router = useRouter();
     useEffect(() => {
         // Check if the user is coming from /auth/new-password
         const referrer = document.referrer;
@@ -27,7 +19,7 @@ const UserButton = () => {
     // get user
     // if user is logged in, return logout button
     // if user is not logged in, return login button
-    const { data: session, status } = useSession();    
+    const session = useCurrentUser();   
     const [showRegister, setShowRegister] = useState(true);
     const isLoading = status === "loading";
     const [open, setOpen] = useState(false);
@@ -52,18 +44,18 @@ const UserButton = () => {
         {!session ? (
             <Link href='/auth/register'>
                 <Button variant="gold" className="mt-5" >
-                    Register
+                    register
                 </Button>
             </Link>
 
         ): (
             <Link href='/settings'>
-                {!session.user.image ? (
+                {!session.image ? (
                     <Avatar alt='logo' className="shadow-xl shadow-black hover:scale-110 transition-transform duration-500  bg-gradient-to-r from-blue-500/90 to-blue-700/90" sx={{ width: 45, height: 45}}>
                     <IoMdPerson className="text-2xl"/>
             </Avatar>
                 ): (
-                    <Avatar alt='logo' src={session.user.image} className="shadow-md shadow-black border-2 border-transparent hover:border-blue-500/50" sx={{ width: 45, height: 45 }}/>
+                    <Avatar alt='logo' src={session.image} className="shadow-md shadow-black border-2 border-transparent hover:border-blue-500/50" sx={{ width: 45, height: 45 }}/>
                 )}
                 
             </Link>
