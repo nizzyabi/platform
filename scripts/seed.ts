15022f63d0aa614492090784b1c6f881bc156eda
NizarAbiZaher <Nizabizaher@gmail.com>
2024-01-19 06:49:35 -0500

    seed.ts

---
+++ scripts/seed.ts
@@ -0,0 +1,4 @@
+// For categories (it could be lessons or topics that you want to display but this would be good for the search bar)
+
+const { PrismaClient } = require("@prisma/client");
+const database = new PrismaClient();
