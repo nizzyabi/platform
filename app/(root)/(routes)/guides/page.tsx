02cc72b7abbe3a9874e407beb73c8e51a1eb9124
NizarAbiZaher <Nizabizaher@gmail.com>
2024-01-19 07:43:01 -0500

    create button

--- app/(root)/(routes)/guides/page.tsx
+++ app/(root)/(routes)/guides/page.tsx
@@ -1,10 +1,25 @@
+'use client'
 import Guides from '@/components/Guides'
+import { Button } from '@/components/ui/button';
+import { useCurrentUser } from '@/hooks/user-current-user';
+import Link from 'next/link';
 import React from 'react'
 
 export default function page() {
+  const session = useCurrentUser();
   return (
     <div>
-
+      {session?.id === 'clrgbzdt20000yma9n65ry5wy' ? (
+        <div>
+          <Link href='/guides/add' className='flex items-center justify-center'>
+            <Button variant="gold" className="mt-5">Create</Button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          
+        </div>
+      )}
       <Guides />
 
     </div>
