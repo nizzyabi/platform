1204cb9ee0ced7c8820496eae39b56926b858182
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2024-01-31 07:57:50 -0500

    working command and search bar

--- components/courses.tsx
+++ components/courses.tsx
@@ -1,13 +1,24 @@
 // Courses Page
 'use client'
-import Image from "next/image";
+import * as React from "react"
 import Link from "next/link";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
-import { Card, CardDescription, CardFooter, CardHeader } from "./ui/card";
 import { useCurrentUser } from "@/hooks/user-current-user";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+
 
 // TODO: Remove this and replace with course creation data
 const guides = [
@@ -60,6 +71,11 @@ export default function Courses() {
   }, []);
   const session = useCurrentUser();
   const nizzyuser = session?.id === 'clrulbr1h0000x3js9ldvplex';
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  }
 
     return (
       <div >
@@ -68,13 +84,34 @@ export default function Courses() {
               <h1 className="font-bold text-7xl pt-3 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">Courses</h1>
               <p className="text-xl font-medium text-gray-200"></p>
           </div>
-
+          {/* special create button */}
           {nizzyuser && 
             <Link href='/courses/add' className="flex items-center justify-center">
               <Button variant="goldHover">Create</Button>
             </Link>
           }
 
+          <div className="flex items-center justify-center pt-5">
+          <>
+            <Input onClick={handleClick} className="bg-[#2e2e2e] rounded cursor-pointer max-w-sm border-slate-100/50" placeholder="Search for a course" />
+            
+            <CommandDialog open={open} onOpenChange={setOpen}>
+              
+              <CommandInput placeholder="NextJS Full Stack" className="border-slate-100/50 w-[400px]  placeholder:text-white/50"/>
+              <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading="All Courses">
+                  <CommandItem>NextJS</CommandItem>
+                  <CommandItem>Focus & Deep Work</CommandItem>
+                  <CommandItem>JavaScript</CommandItem>
+                </CommandGroup>
+              </CommandList>
+            </CommandDialog>
+            
+          </>
+          </div>
+          
+          
            
       </div>
     )
--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -15,7 +15,7 @@ const Command = React.forwardRef<
   <CommandPrimitive
     ref={ref}
     className={cn(
-      "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
+      "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground border-slate-100/50",
       className
     )}
     {...props}
@@ -29,7 +29,7 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+        <Command className="border border-slate-100/30 rounded [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5 bg-[#2e2e2e]">
           {children}
         </Command>
       </DialogContent>
@@ -41,12 +41,12 @@ const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
 >(({ className, ...props }, ref) => (
-  <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
-    <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+  <div className="flex items-center border-b border-slate-100/30 px-3" cmdk-input-wrapper="">
+    <Search className="mr-5 shrink-0 opacity-50" />
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
-        "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+        "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground/50 disabled:cursor-not-allowed disabled:opacity-50 border-slate-100/50",
         className
       )}
       {...props}
@@ -104,7 +104,7 @@ const CommandSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <CommandPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 h-px bg-border", className)}
+    className={cn("-mx-1 h-px bg-slate-100/50", className)}
     {...props}
   />
 ))
@@ -117,7 +117,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 border-slate-100/50",
       className
     )}
     {...props}
