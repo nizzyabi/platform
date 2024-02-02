67621523ff63b7b4c398d43f0c8de4f09cbeaa04
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2024-02-02 07:51:43 -0500

    video upload working!

--- app/(root)/(routes)/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(root)/(routes)/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -7,8 +7,7 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Chapter, MuxData } from "@prisma/client";
-import Image from "next/image";
-
+import MuxPlayer from "@mux/mux-player-react";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
 
@@ -72,8 +71,10 @@ export const ChapterVideoForm = ({
             <VideoIcon className="h-10 w-10 text-slate-500" />
           </div>
         ) : (
-          <div className="relative aspect-video mt-2">
-            Video Uploaded!
+          <div className="relative aspect-video mt-2 ">
+            <MuxPlayer 
+              playbackId={initialData?.muxData?.playbackId || ""}
+            />
           </div>
         )
       )}
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,12 @@
 import { auth } from "@/auth";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
+import Mux from "@mux/mux-node";
 
+const { Video } = new Mux(
+    process.env.MUX_TOKEN_ID!,
+    process.env.MUX_TOKEN_SECRET!
+);
 export async function PATCH(
     req:Request,
     { params } : { params: { courseId: string; chapterId: string } }
@@ -34,8 +39,38 @@ export async function PATCH(
                 ...values
             }
         })
-        console.log("Success")
-        // TODO: Handle Video Upload
+        
+        if (values.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId
+                }
+            });
+
+            // if user changes video, deleted last one
+            if(existingMuxData) {
+                await Video.Assets.del(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id
+                    }
+                })
+            }
+
+            const asset = await Video.Assets.create({
+                input: values.videoUrl,
+                playback_policy: "public",
+                test: false,
+            });
+
+            await db.muxData.create({
+                data: {
+                    chapterId: params.chapterId,
+                    assetId: asset.id,
+                    playbackId: asset.playback_ids?.[0]?.id
+                }
+            })
+        }
         return NextResponse.json(chapter);
     } catch (error) {
         console.log("[COURSES_CHAPTER_ID]", error);
