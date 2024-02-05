029489060cfe0cd1c9bb8d4a6c55597aa6fe9792
NizarAbiZaher <Nizabizaher@gmail.com>
2024-02-05 06:51:31 -0500

    working link to course to edit

--- app/(root)/(routes)/courses/_components/colums.tsx
+++ app/(root)/(routes)/courses/_components/colums.tsx
@@ -3,7 +3,14 @@
 import { Button } from "@/components/ui/button"
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown, MoreHorizontal, Wand2 } from "lucide-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link"
 
 export const columns: ColumnDef<Course>[] = [
   {
@@ -22,10 +29,58 @@ export const columns: ColumnDef<Course>[] = [
   },
   {
     accessorKey: "price",
-    header: "Price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+        <ArrowUpDown className="ml-2 h-4 w-4"/>
+        </Button>
+      )
+    }
   },
   {
     accessorKey: "isPublished",
-    header: "Published",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Published
+          <ArrowUpDown className="ml-2 h-4 w-4"/>
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      
+    }
   },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-4 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4"/>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end' className="rounded border-slate-100/30 bg-[#191919]">
+            <Link href={`/courses/${id}`}>
+              <DropdownMenuItem className=" rounded bg-[#191919] cursor-pointer">
+                <Wand2 className="h-4 w-4 mr-2"/>
+                Edit
+              </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>      
+      )
+    }
+  }
 ]
