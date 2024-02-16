de71148010e48baafb71c65349db81571d734409
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2024-02-16 09:50:39 -0500

    coaching page update

--- components/Coaching.tsx
+++ components/Coaching.tsx
@@ -5,6 +5,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 
 
 export default function Coaching() {
@@ -19,19 +20,26 @@ export default function Coaching() {
     <div>
         {/* Title */}
         <div className="text-center pb-5 pt-[200px]" data-aos='fade-left'>
-            <h1 className="font-extrabold text-3xl md:text-5xl pt-3">1-on-1 Coaching</h1>
-            <p className="text-lg md:text-xl pt-1">Get personal advice, mentorship, & guidance to learn code faster</p> 
+            <h1 className="font-bold text-7xl md:text-5xl pt-3">Mentorship</h1>
+            <p className="text-lg md:text-xl pt-1">Get personal advice, mentorship, & guidance to learn code faster!</p> 
         </div>
 
-        {/* Coaching */}
-        <div data-aos='fade-right' className="space-y-20 pb-[200px] flex items-center justify-center">
+        
+        <div data-aos='fade-right' className="space-y-20 pb-[200px] grid grid-cols-3 px-8 pt-5">
           {/* Link to coaching */}
           
-            <Button variant='pinkHover'>
-              <Link href='https://xpykzd2a2ce.typeform.com/to/r6Bjujqi' className="">
-                Apply Now
-              </Link>
-            </Button>
+            <Card className="w-[350px]">
+              <CardHeader>
+                <CardTitle>Monthly</CardTitle>
+                <CardDescription>Get personal advice on a monthly basis through live sessions, calls, and texts</CardDescription>
+              </CardHeader>
+              <CardContent></CardContent>
+              <CardFooter className="flex justify-between">
+                <Button variant="goldHover">
+                  Register
+                </Button>
+              </CardFooter>
+            </Card>
           
         </div>
 
