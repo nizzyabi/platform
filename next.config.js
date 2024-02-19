ee27c55633fb6ab6d79dcdbf5b118a0b7b7c3dbc
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2024-02-19 15:10:31 -0500

    fixed images domain

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "utfs.io"
+        remotePatterns: [
+            {
+                hostname:"utfs.io"
+            }
         ]
     }
 }
