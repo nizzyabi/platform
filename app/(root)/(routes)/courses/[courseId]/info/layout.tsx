8203f230709b6d99d2bea3b1c2ca87efcd6da3ca
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2024-02-24 10:37:17 -0500

    card update

--- app/(root)/(routes)/courses/[courseId]/info/layout.tsx
+++ app/(root)/(routes)/courses/[courseId]/info/layout.tsx
@@ -119,19 +119,19 @@ const CourseInfoLayout = async ({
               </div>
             </div>
 
-            <div>
-              <h1 className="text-center text-5xl font-semibold my-8">Chapters</h1>
-              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  justify-items-center">
-                {course?.chapters.map((chapter) => (
-                  <Link href={`/course/${course?.id}/chapter/${chapter.id}`} key={chapter.id}>
-                    <div className="bg-[#131212] shadow-lg shadow-black/50 rounded-xl w-[300px] p-6 m-2 flex flex-col hover:bg-[#1a1a1a] transition duration-300 ease-in-out">
-
-                    <h2 className="text-2xl font-bold  text-white mb-4">{chapter.title}</h2>
-          
-                    <p className="opacity-50">{chapter.description}</p>
-                    </div>
-                  </Link>
-                ))}
+            <div className="flex justify-center">
+              <div className="flex flex-col items-center">
+                <h1 className="text-center text-5xl font-semibold my-8">Chapters</h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                  {course?.chapters.map((chapter) => (
+                    <Link href={`/course/${course?.id}/chapter/${chapter.id}`} key={chapter.id}>
+                      <div className="bg-[#131212] shadow-lg shadow-black/50 rounded-xl w-[300px] p-6 m-4 flex flex-col hover:bg-[#1a1a1a] transition duration-300 ease-in-out">
+                        <h2 className="text-2xl font-bold text-white mb-4">{chapter.title}</h2>
+                        <p className="opacity-50">{chapter.description}</p>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
             
