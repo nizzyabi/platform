29821ef43a6d2e02a8b72cfc7f5f77f2c4e4bf82
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2024-02-25 09:20:50 -0500

    landing page testimonial data

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -9,6 +9,49 @@ import { LanguageBackground } from './designs/bg-gradient';
 import { BentoGridThirdDemo } from './bento';
 import { ThreeD } from './3d';
 
+const testimonials = [
+  {
+    username: 'paulos',
+    avatar: 'https://randomuser.me',
+    message: "I have been learning to code for a while now, and I have to say that Nizar's channel has been a great help. The discord community is so helpful, and the content is top-notch :)."
+  },
+  {
+    username: 'bacio001',
+    avatar: 'https://randomuser.me',
+    message: "Nizar helped me keep motivation in my own startup, and his videos are also nice to listen to while coding."
+  },
+  {
+    username: 'liambrewster',
+    avatar: 'https://randomuser.me',
+    message: "Nizar’s breakdown of project building in live streams and videos is simply brilliant. His clear, easy-to-follow approach has made a significant impact on my understanding and confidence to get building! Highly recommended!"
+  },
+  {
+    username: 'JonasDieNuss',
+    avatar: 'https://randomuser.me',
+    message: "Nizar on YT has been a game-changer for me! His educational and entertaining videos really helped me get back into coding, improve my skills, and establish better routines. I owe a lot of my progress to his guidance and suggestions."
+  },
+  {
+    username: 'gorlami',
+    avatar: 'https://randomuser.me',
+    message: "Nizar's content consistently delivers valuable insights and entertainment in equal measure. Proud to be a part of your community!"
+  },
+  {
+    username: 'amanbhujel',
+    avatar: 'https://randomuser.me',
+    message: "After watching Nizar's videos, I've been inspired to pursue my own business venture, and I'm confident he'll become a remarkable entrepreneur and influencer."
+  },
+  {
+    username: 'Tiernan DeFranco',
+    avatar: 'https://randomuser.me',
+    message: "Nizar's channel came into my recommended shortly after I had finished learning basic web development, and despite originally attempting to leverage my skills in a regular career, his videos showcasing him building his startup pulled me back into pursuing an idea I had semi-given up on."
+  },
+  {
+    username: 'Solomon.chidera',
+    avatar: 'https://randomuser.me',
+    message: "Discovered this server on YouTube, and it's been a pleasant surprise. The community here is so welcoming, and the content is genuinely helpful, unlike some others out there just looking to grab your attention and money."
+  }
+]
+
 export default function LandingPage() {
   useEffect(() => {
     AOS.init({
