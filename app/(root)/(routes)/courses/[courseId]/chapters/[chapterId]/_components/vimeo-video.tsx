6f32b4b3bfcfdd318a6fd1e3cb1630e2b38c80a1
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2024-03-04 06:58:14 -0500

    vimeo svaed to database

---
+++ app/(root)/(routes)/courses/[courseId]/chapters/[chapterId]/_components/vimeo-video.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Chapter } from "@prisma/client";
+import AutoFixNormalIcon from '@mui/icons-material/AutoFixNormal';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Editor } from "@/components/editor";
+import { init } from "aos";
+import { Preview } from "@/components/preview";
+import { Input } from "@/components/ui/input";
+
+interface VimeoVideoFormProps {
+  initialData: Chapter;
+  courseId: string;
+  chapterId: string;
+};
+
+const formSchema = z.object({
+  vimeoVideo: z.string().min(1),
+});
+
+export const VimeoVideoForm = ({
+  initialData,
+  courseId,
+  chapterId
+}: VimeoVideoFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+        vimeoVideo: initialData?.vimeoVideo || ""
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}`, values);
+      toast.success("Chapter updated");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    }
+  }
+
+  return (
+    <div className="mt-6 border border-slate-100/20 shadow-md bg-[#1e1e1e] bg-opacity-95 rounded-xl p-4">
+      <div className="font-semibold flex items-center justify-between text-xl">
+        Vimeo Video Link
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <AutoFixNormalIcon className="text-slate-200" />
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p className="text-sm mt-2">
+          {initialData.vimeoVideo}
+        </p>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="vimeoVideo"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl className="relative rounded bg-slate-100 text-[#2e2e2e]">
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Introduction to the course'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              disabled={!isValid || isSubmitting}
+              type="submit"
+              className=" flex"
+              variant="basic"
+            >
+              Save
+            </Button>
+          </form>
+        </Form>
+      )}
+    </div>
+  )
+}
