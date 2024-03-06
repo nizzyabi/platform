80d677d6f5dfbbc53537a0b9abc7d68073b648ca
NizarAbiZaher <Nizabizaher@gmail.com>
2024-03-05 19:39:27 -0500

    course purchase

--- app/(course)/course/[courseId]/chapter/[chapterId]/_components/course-purchase-button.tsx
+++ app/(course)/course/[courseId]/chapter/[chapterId]/_components/course-purchase-button.tsx
@@ -32,7 +32,7 @@ export const CoursePurchaseButton = ({
     }
     return (
           
-        <Button className="relative inline-flex h-12 overflow-hidden rounded-xl p-[1px] hover:opacity-50 ">
+        <Button onClick={onClick}className="relative inline-flex h-12 overflow-hidden rounded-xl p-[1px] hover:opacity-50 ">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-[#191919] px-3 py-1 text-md font-bold text-white backdrop-blur-3xl">
         Purchase course for {formatPrice(price)}
