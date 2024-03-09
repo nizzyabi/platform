8dccf77120a31dfeb692530f9ef83e1aced68b9d
NizarAbiZaher <Nizabizaher@gmail.com>
2024-03-09 10:20:05 -0500

    routing for deployment

--- lib/mail.ts
+++ lib/mail.ts
@@ -7,7 +7,7 @@ export const sendVerificationEmail = async (
     email: string,
     token: string
 ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `https://codep.vercel.app//auth/new-verification?token=${token}`;
 
     await resend.emails.send({
         from: "Nizar <noreply@nizarlanding.com>",
