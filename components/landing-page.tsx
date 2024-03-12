a46c1cd24f4c7aa97e02728741f46b53d9482f83
NizarAbiZaher <Nizabizaher@gmail.com>
2024-03-11 22:20:25 -0400

    button

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -35,9 +35,9 @@ export default function LandingPage() {
         <div className='flex-1 flex flex-col items-center justify-center mb-4'>
             <h1 className='text-7xl text-slate-100 text-center pt-3 font-bold header-landing'>Learn To<span className='block text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500'> Code</span></h1>
             <p className='pt-3 font-medium text-xl text-gray-200'>&& have fun doing it</p>
-            <Button variant='brand' size='brand' className='mt-3'>
+            <Button variant='default' className='font-medium py-2 mt-3 '>
               <Link href={session ? '/courses' : '/auth/register'} className='flex'>
-                Let's Start <Wand2 className='ml-2 h-4 w-4' />
+                Get started
               </Link>
             </Button>
         </div>
@@ -49,8 +49,8 @@ export default function LandingPage() {
         <div className='pt-64'>
           <div className='text-center'>
             <h1 className='text-7xl text-slate-100 text-center pt-3 font-semibold header-landing'>Learn Modern Day<span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-blue-500 block '> Languages</span></h1>
-            <Button variant='brand' size='brand' className='mt-5 mb-8'>
-              <Link href={session ? '/courses' : '/auth/register'}>
+            <Button variant='default' className='font-semibold py-2 mt-5 mb-8'>
+              <Link href={session ? '/courses' : '/auth/register'} className='flex'>
                 Let's Start
               </Link>
             </Button>
@@ -65,8 +65,8 @@ export default function LandingPage() {
         <div>
           <div className='text-center pt-64'>
             <h1 className='text-7xl text-slate-100 text-center pt-3 font-semibold header-landing'>Hear It From<span className='text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-yellow-500 block'> Others</span></h1>
-            <Button variant='brand' size='brand' className='mt-5 '>
-              <Link href={session ? '/courses' : '/auth/register'}>
+            <Button variant='default' className='font-semibold py-2 mt-5'>
+              <Link href={session ? '/courses' : '/auth/register'} className='flex'>
                 Let's Start
               </Link>
             </Button>
