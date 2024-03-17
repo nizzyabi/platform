66a9de1f0777d4ed66afc8bd3707ec0241a8c54d
NizarAbiZaher <Nizabizaher@gmail.com>
2024-03-17 09:12:39 -0400

    mail routing fixes

--- lib/mail.ts
+++ lib/mail.ts
@@ -7,7 +7,7 @@ export const sendVerificationEmail = async (
     email: string,
     token: string
 ) => {
-    const confirmLink = `https://codep.vercel.app//auth/new-verification?token=${token}`;
+    const confirmLink = `https://nizzyabi.com/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
         from: "Nizar <noreply@nizarlanding.com>",
@@ -28,7 +28,7 @@ export const sendPasswordResetEmail = async (
     email: string,
     token: string,
 ) => {
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+    const resetLink = `http://nizzyabi.com/auth/new-password?token=${token}`;
 
     await resend.emails.send({
         from: "onboarding@resend.dev",
