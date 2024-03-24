9fee7ea2c24b007bf038ceffde2dc3e5fb48e8e8
NizarAbiZaher <Nizabizaher@gmail.com>
2024-03-24 10:21:53 -0400

    fixed video

--- app/(course)/course/[courseId]/chapter/[chapterId]/page.tsx
+++ app/(course)/course/[courseId]/chapter/[chapterId]/page.tsx
@@ -68,7 +68,7 @@ const ChapterIdPage = async ({
     
 
   return (
-    <div className="w-full px-5 overflow-hidden mb-5">
+    <div className="w-full px-5 lg:pr-12 overflow-hidden mb-5">
         <div>
             <iframe
              src={`${chapter.vimeoVideo}`} 
