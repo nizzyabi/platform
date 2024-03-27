8a8795891f57bfb064a4fb271aae2686a774d574
NizarAbiZaher <Nizabizaher@gmail.com>
2024-03-27 08:09:16 -0400

    color bg hover to zinc-100/30

--- components/mentorship.tsx
+++ components/mentorship.tsx
@@ -4,6 +4,9 @@ import { ArrowRight } from "lucide-react";
 import { Separator } from "./ui/separator";
 import Image from "next/image";
 import { AlertTriangle, Calendar, GitMerge, Rocket } from "lucide-react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const features = [
   {
@@ -29,12 +32,19 @@ const features = [
 ];
 
 export const Mentorship: React.FC = () => {
+  useEffect(() => {
+    AOS.init({
+      disable: "phone",
+      duration: 800,
+      easing: "ease-out-cubic",
+    });
+  }, []);
   return (
     <div className="landing pb-40 overflow-hidden">
       <div className=" px-4 mx-auto sm:px-6">
         <div>
           {/* Content */}
-          <div className="text-center pb-5 pt-40">
+          <div className="text-center pb-5 pt-40" data-aos='fade-up'>
             <h1 className="font-bold text-7xl mx-6"> 1-on-1 Tutoring</h1>
             <p className="text-md text-slate-100/40 pt-1">Get personal advice, mentorship, & guidance to learn code faster!</p> 
             <div className="flex items-center justify-center">
@@ -49,12 +59,7 @@ export const Mentorship: React.FC = () => {
                   <div>
                   {/* Section content */}
                     <div className="flex flex-col max-w-xl mx-auto md:max-w-none md:flex-row md:space-x-8 lg:space-x-16 xl:space-x-20 space-y-8 space-y-reverse md:space-y-0">
-                      <div className="order-1 md:w-7/12 lg:w-1/2 md:order-none max-md:text-center">
-                        <div>
-                          <div className="inline-flex pb-3 font-medium text-transparent bg-clip-text bg-gradient-to-r from-primary-500 to-primary-200">
-                            Don't drown in alerts
-                          </div>
-                        </div>
+                      <div className="order-1 md:w-7/12 lg:w-1/2 md:order-none max-md:text-center" data-aos='fade-right'>
                         <h3 className="pb-3 text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-violet-500  to-pink-500">
                           Why Tutor With Nizar?
                         </h3>
@@ -65,15 +70,15 @@ export const Mentorship: React.FC = () => {
                           {features.map((feature) => (
                             <div
                               key={feature.name}
-                              className="px-2 py-1 rounded group hover:bg-zinc-100 duration-500"
+                              className="px-2 py-1 rounded group hover:bg-zinc-100/20  transition duration-500 cursor-pointer"
                             >
                               <div className="flex items-center mb-1 space-x-2 ">
-                                <feature.icon className="w-4 h-4 shrink-0 text-zinc-300 group-hover:text-zinc-950 duration-500" />
-                                <h4 className="font-medium text-zinc-50 group-hover:text-zinc-950 duration-500">
+                                <feature.icon className="w-4 h-4 shrink-0 text-zinc-300" />
+                                <h4 className="font-medium text-zinc-50">
                                   {feature.name}
                                 </h4>
                               </div>
-                              <p className="text-sm text-left text-zinc-400 group-hover:text-zinc-950 duration-500">
+                              <p className="text-sm text-left text-zinc-300">
                                 {feature.description}
                               </p>
                             </div>
@@ -82,7 +87,7 @@ export const Mentorship: React.FC = () => {
                       </div>
 
                       <div className="flex max-w-2xl mx-auto mt-16 md:w-5/12 lg:w-1/2 sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-                        <div className="z-10 flex-none max-w-3xl sm:max-w-5xl lg:max-w-none">
+                        <div className="z-10 flex-none max-w-3xl sm:max-w-5xl lg:max-w-none" data-aos='fade-left'>
                           <Image
                             src="voice.svg"
                             alt="App screenshot"
