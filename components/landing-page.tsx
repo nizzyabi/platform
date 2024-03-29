f0ba818be8053cdba4e930065b48f49d9a34759a
NizarAbiZaher <Nizabizaher@gmail.com>
2024-03-28 22:22:48 -0400

    updated button

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -38,7 +38,7 @@ export default function LandingPage() {
               target="_blank"
             >
               <span className="relative inline-flex items-center">
-                Want to contribute? Checkout the Github Repo {" "}
+                Github repository {" "}
                 <span className="tracking-normal text-primary-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
                   -&gt;
                 </span>
