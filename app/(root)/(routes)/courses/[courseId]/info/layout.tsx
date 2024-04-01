ff2bf663bfa679fa896a3994e4766c76dc9ce152
NizarAbiZaher <Nizabizaher@gmail.com>
2024-04-01 08:34:32 -0400

    chapter description 2 changes

--- app/(root)/(routes)/courses/[courseId]/info/layout.tsx
+++ app/(root)/(routes)/courses/[courseId]/info/layout.tsx
@@ -51,7 +51,7 @@ const CourseInfoLayout = async ({
     });
     
     return (
-        <div className="pt-40 mx-20 pb-40 space-y-10 text-center landing">
+        <div className="pt-40 mx-20 pb-40 space-y-20 text-center landing">
           <div className="text-center pb-8">
             <h1 className="text-6xl font-extrabold mb-3">{course?.title}</h1>
             <p className="text-sm opacity-50 font-medium ">{course?.description}</p>
@@ -77,23 +77,15 @@ const CourseInfoLayout = async ({
                 <ArrowRight className="w-3 h-3 tracking-normal text-primary-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1" />
               </Link>
             </div>
-
-            <div className="flex items-center justify-center">
-                <Separator className="mt-8 bg-slate-100/20 h-0.5 w-20 mb-8" />
-            </div>
           </div>
 
           <div>
-            {course?.description2}
-          </div>
-
-          <div>
-            <h1 className="text-5xl font-bold ">What will I learn?</h1>
+            <h1 className="text-4xl font-bold ">What will I learn?</h1>
             {course?.learningOutcome}            
           </div>
 
           <div>
-            <h1 className="text-5xl font-bold ">Tech stack we will use</h1>
+            <h1 className="text-4xl font-bold ">What will we build?</h1>
               <div className="flex items-center justify-center pb-4 pt-5 space-x-5">
                 <Image 
                   src={`${course?.language}`} 
@@ -118,20 +110,21 @@ const CourseInfoLayout = async ({
                   
                 />
               </div>
+              {course?.description2}
           </div>
 
           <div className="mx-auto max-w-2xl">
-            <h1 className="text-5xl font-bold ">What's included?</h1>
+            <h1 className="text-4xl font-bold ">What's included?</h1>
             {course?.included}
           </div>
 
           <div className="mx-auto max-w-2xl">
-            <h1 className="text-5xl font-bold">Difficulty level</h1>
+            <h1 className="text-4xl font-bold">Difficulty level</h1>
             {course?.difficulty}
           </div>
 
           <div className="mx-auto max-w-2xl">
-            <h1 className="text-5xl font-bold">Purchase course</h1>
+            <h1 className="text-4xl font-bold">Purchase course</h1>
             {
               !course?.price
               ? <p>This course is free!</p>
@@ -144,10 +137,10 @@ const CourseInfoLayout = async ({
           <div className="flex justify-center">
             <div className="flex flex-col items-center">
               <h1 className="text-center text-5xl font-bold my-8">Chapters</h1>
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
                 {course?.chapters.map((chapter, index) => (
                   <Link href={`/course/${course?.id}/chapter/${chapter.id}`} key={chapter.id}>
-                    <div className="bg-zinc-900 shadow-lg shadow-black/50 rounded-[5px]  w-full p-6 m-4 flex flex-col hover:bg-opacity-80 transition duration-300 ease-in-out">
+                    <div className="bg-zinc-900 shadow-lg shadow-black/50 rounded-[5px]  w-full p-6  flex flex-col hover:bg-opacity-80 transition duration-300 ease-in-out">
                       <div className="flex">
                         <h2 className="text-xl font-bold text-white mb-4"><span className="mr-2 font-extrabold text-2xl opacity-40">0{index + 1}  </span>{chapter.title}</h2>
                       </div>
