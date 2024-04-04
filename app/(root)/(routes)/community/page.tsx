b356101f33f1c6ac9cbfd23ce167160ca570fe17
NizarAbiZaher <Nizabizaher@gmail.com>
2024-04-04 18:50:02 -0400

    dialog

--- app/(root)/(routes)/community/page.tsx
+++ app/(root)/(routes)/community/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
@@ -13,21 +14,23 @@ export default function page() {
       <div className="flex items-center justify-center">
         <Dialog>
           <DialogTrigger>
-            <div className="flex items-center justify-center bg-slate-100 h-12 rounded-[5px] text-[#121212] pr-40 pl-3 py-7">
-              <Avatar src={`${session?.image}`} className="mr-2 w-8 h-8"/>
-              <p className="font-medium">Write something...</p>
+            <div className="flex items-center justify-center bg-slate-100 h-12 rounded-[5px] text-[#121212]  mx-40 pr-80 pl-3 py-7">
+              <Avatar src={`${session?.image}`} className="mr-2"/>
+              <p className="font-medium no-wrap">Write something...</p>
             </div>
           </DialogTrigger>
-          <DialogContent className="bg-slate-100 text-[#191919] rounded-[5px] w-[400px]" >
+          <DialogContent className="bg-slate-100 text-[#191919] rounded-[5px] sm:w-[400px] md:w-[500px] mx-auto" >
             <DialogHeader>
               <DialogTitle>
-                <Input placeholder="Title" className="py-0"/>
+                <Input placeholder="Title" className="py-0 placeholder:text-3xl text-3xl"/>
               </DialogTitle>
               <DialogDescription className="text-gray-400">
-              <Textarea placeholder="Write something..." className="border-none text-[#191919] placeholder:text-gray-400"/>
+              <Textarea placeholder="Write something..." className="border-none text-[#191919] placeholder:text-gray-400 text-md placeholder:text-md"/>
               </DialogDescription>
             </DialogHeader>
-            
+            <div className="flex-end">
+              <Button className="bg-[#191919] text-slate-100 hover:opacity-90">Post</Button>
+            </div>
           </DialogContent>
         </Dialog>
       </div>
