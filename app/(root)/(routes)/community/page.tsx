c3c546baca4ef49baaf985b28c7c59fa45b11c45
NizarAbiZaher <Nizabizaher@gmail.com>
2024-04-04 16:24:44 -0400

    create community room

--- app/(root)/(routes)/community/page.tsx
+++ app/(root)/(routes)/community/page.tsx
@@ -1,7 +1,7 @@
 export default function page() {
   return (
-    <div className='landing'>
-      {/* Open sourced project, discord community link, etc. */}
+    <div className='landing flex min-h-screen flex-col items-center justify-center py-1'>
+      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-orange-500">Coming Soon...</h1>
     </div>
   )
 }
--- app/(root)/(routes)/create-room/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function CreateRoomPage() {
-    return (
-        <div className="pt-40">
-        Create Room
-        </div>
-    )
-}
