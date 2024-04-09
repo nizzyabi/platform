5f40cb5b95858f801d2f896b1bdc04011eaf82bb
NizarAbiZaher <Nizabizaher@gmail.com>
2024-04-09 10:13:33 -0400

    successurl

--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -82,7 +82,7 @@ export async function POST(
             customer: stripeCustomer.stripeCustomerId,
             line_items,
             mode: 'payment',
-            success_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}`,
+            success_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}/success=1`,
             cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}/canceled=1`,
             metadata: {
                 courseId: course.id,
